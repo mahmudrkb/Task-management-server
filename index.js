62c1724826e77ee2aea2b04a64d63cc2bd44d2f8
mahmudrkb <almahmudrkb98@gmail.com>
2025-02-21 01:18:05 +0600

    show task  in card

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ async function run() {
         const result = await taskCollections.insertOne(task);
         res.send(result);
       });
+
+      app.get("/show-task/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email : email };
+        const result = await taskCollections.find(query).toArray();
+        res.send(result);
+      });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
